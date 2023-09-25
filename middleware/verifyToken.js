f72f4e9f24864b062fa0107cd486b28277e1764a
Sai Kumar Kanuri <saikumar.kanuri.ksvs@gmail.com>
2023-09-25 08:02:10 +0530

    resolved some bugs

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,10 +1,27 @@
 const jwt = require("jsonwebtoken");
 const handleError = require("./error");
 const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
+    // const token = req.cookies.access_token;
 
-    if (!token) {
-        next(handleError(401, "Your are not Authenticated"));
+    // if (!token) {
+    //     next(handleError(401, "Your are not Authenticated"));
+    // }
+
+    let token;
+
+    // Check if the token is present in cookies
+    if (req.cookies && req.cookies.access_token) {
+        token = req.cookies.access_token;
+    }
+
+    // If not found in cookies, check the "Authorization" header
+    if (!token && req.headers.authorization) {
+        const authHeader = req.headers.authorization;
+        const tokenParts = authHeader.split(' ');
+
+        if (tokenParts.length === 2 && tokenParts[0] === 'Bearer') {
+            token = tokenParts[1];
+        }
     }
 
     jwt.verify(token, process.env.JWT, (err, user) => {
